240eb060b3b0df5c95506d9936c8249ba0b8ddce
garrettkeefe75 <57542053+garrettkeefe75@users.noreply.github.com>
2019-11-11 16:43:11 -0700

    Add files via upload

--- Timing.java
+++ Timing.java
@@ -6,12 +6,23 @@ import org.junit.Before;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * 
+ * @author Garrett Keefe & Joshua Wells
+ * 
+ * Timing Class for Assignment 08
+ *
+ */
+
 public class Timing {
 
     private HashTableSet<Integer> table;
     private ArrayList<Integer> tableSize;
     private int itemInTable;
-
+    
+/**
+ * Initializes the required variables and tables for use
+ */
     @Before
     public void setUp(){
         table = new HashTableSet<>();
@@ -21,7 +32,13 @@ public class Timing {
         }
 
     }
-
+    
+/**
+ * creates a table for usage in testing .contains
+ * 
+ * @param size
+ * @return Table of Size {int size}
+ */
     private HashTableSet<Integer> createTable(int size){
         table = new HashTableSet<Integer>();
         for(int i = 0; i < size; i++){
@@ -32,6 +49,12 @@ public class Timing {
         return table;
     }
 
+    /**
+     * creates a table for usage in testing .contains
+     * 
+     * @param size
+     * @return Table of Size {int size} with buckets {int buckets}
+     */
     private HashTableSet<Integer> createTable(int size, int buckets){
         table = new HashTableSet<Integer>(buckets);
         for(int i = 0; i < size; i++){
@@ -42,6 +65,11 @@ public class Timing {
         return table;
     }
 
+    /**
+     * test contains on given hash table x
+     * @param x
+     * @return time required to run contains
+     */
     private long getContainsTime(HashTableSet<Integer> x) {
         Random rand = new Random();
         long start = System.nanoTime();
@@ -51,6 +79,12 @@ public class Timing {
         return stop - start;
     }
 
+    /**
+     * Calculates an average of amount of time to run contains on each size
+     * @param x
+     * @param times
+     * @return Average of the timing for contains
+     */
     private long averageTime(HashTableSet<Integer> x, long times){
         long average = 0L;
         for (int i = 0; i < times; i++){
@@ -59,6 +93,9 @@ public class Timing {
         return average / times;
     }
 
+    /**
+     * Method that tests contains on tables of various sizes and bucket sizes.
+     */
     @Test
     public void times(){
         for (int num :
